6a8432a4ebe6d1ba5f0852e10cce283dd0dd4b4f
tobias <tobias@littlegeek.local>
2009-11-19 21:57:57 +0100

    Corrected typo.

--- src/typhoonae/xmpp/tests/__init__.py
+++ src/typhoonae/xmpp/tests/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Python popyright 2009 Tobias Rodäbel
+# Copyright 2009 Tobias Rodäbel
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

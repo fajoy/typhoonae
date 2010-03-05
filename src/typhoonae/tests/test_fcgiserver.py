5ec992244543b38b841f81d68c7ad0e56871ca9c
tobias <tobias@littlegeek.local>
2010-03-05 19:36:39 +0100

    Added unit tests.

--- src/typhoonae/tests/test_fcgiserver.py
+++ src/typhoonae/tests/test_fcgiserver.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 Tobias Rodäbel
+# Copyright 2009, 2010 Tobias Rodäbel
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@
 import StringIO
 import os
 import sys
+import tempfile
 import typhoonae.fcgiserver
 import unittest
 
@@ -31,6 +32,19 @@ class TestCase(unittest.TestCase):
         app_root = os.path.join(os.path.dirname(__file__), 'sample')
         os.chdir(app_root)
         sys.path.insert(0, os.getcwd())
+
+    def testServer(self):
+        """Tests the main FastCGI server loop."""
+
+        import fcgiapp
+
+        class Options():
+            debug_mode = False
+
+        self.assertRaises(
+            fcgiapp.error,
+            typhoonae.fcgiserver.serve,
+            typhoonae.getAppConfig(), Options())
 
     def testRunModule(self):
         """Tries to load and run a python module."""
@@ -53,3 +67,38 @@ class TestCase(unittest.TestCase):
 
         buffer = request('/unknown')
         assert buffer.getvalue().startswith('Status: 404 Not Found')
+
+    def testCGIHandlerChain(self):
+        """Tests if our CGI handler chain works."""
+
+        chain = typhoonae.fcgiserver.CGIHandlerChain((lambda x,y:True))
+        fp = StringIO.StringIO()
+        chain(fp, {})
+        fp.close()
+
+    def testCGIInAdapter(self):
+        """Tests the FastCGI input strem adapter."""
+
+        stdin = StringIO.StringIO('sample data\nfoobar')
+        adapted_stdin = typhoonae.fcgiserver.CGIInAdapter(stdin) 
+        self.assertEqual('sample data\nfoobar', adapted_stdin.read())
+        adapted_stdin.seek(0)
+        self.assertEqual('sample data\n', adapted_stdin.readline())
+        adapted_stdin.close()
+
+    def testCGIOutAdapter(self):
+        """Tests the FastCGI output strem adapter."""
+
+        stdout = StringIO.StringIO()
+        adapted_stdout = typhoonae.fcgiserver.CGIOutAdapter(stdout)
+        adapted_stdout.write('foobar')
+        adapted_stdout.flush()
+
+        class BrokenFastCGIOutStream:
+            def write(self, data):
+                raise IOError
+
+        stdout = BrokenFastCGIOutStream()
+        adapted_stdout = typhoonae.fcgiserver.CGIOutAdapter(stdout)
+        adapted_stdout.write('foobar')
+        adapted_stdout.flush()

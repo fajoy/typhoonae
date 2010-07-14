356dfd5f183e3f845c84623593a8b63e33d4f42c
tobias <tobias@littlegeek.local>
2010-07-14 11:06:35 +0200

    Added unit test for loading CGI scripts.

--- src/typhoonae/tests/test_fcgiserver.py
+++ src/typhoonae/tests/test_fcgiserver.py
@@ -48,8 +48,29 @@ class TestCase(unittest.TestCase):
             typhoonae.fcgiserver.serve,
             typhoonae.getAppConfig(), Options())
 
+    def testLoadModule(self):
+        """Tries to load a CGI script."""
+
+        handler_path = 'app.py'
+        cgi_path = os.path.join(
+            os.path.dirname(__file__), 'sample', handler_path)
+        module_cache = {}
+        typhoonae.fcgiserver.load_module(handler_path, cgi_path, module_cache)
+        self.assertEqual(['app'], module_cache.keys())
+        mod_obj = module_cache['app']
+        typhoonae.fcgiserver.load_module(handler_path, cgi_path, module_cache)
+        self.assertEqual(mod_obj, module_cache['app'])
+        mod_obj = module_cache['app']
+        typhoonae.fcgiserver.load_module(
+            handler_path, cgi_path, module_cache, debug=True)
+        self.assertNotEqual(mod_obj, module_cache['app'])
+
     def testRunModule(self):
         """Tries to load and run a python module."""
+
+        handler_path = 'app.py'
+        cgi_path = os.path.join(
+            os.path.dirname(__file__), 'sample', handler_path)
 
         def request(uri, method='GET'):
             """Fakes a get request."""
@@ -58,9 +79,7 @@ class TestCase(unittest.TestCase):
             sys.stdout = buffer
             os.environ['PATH_INFO'] = uri
             os.environ['REQUEST_METHOD'] = method
-            typhoonae.fcgiserver.run_module(
-                'app.py',
-                os.path.join(os.path.dirname(__file__), 'sample', 'app.py'))
+            typhoonae.fcgiserver.run_module(handler_path, cgi_path, debug=True)
             sys.stdout = sys.__stdout__
             del os.environ['PATH_INFO']
             del os.environ['REQUEST_METHOD']

2731c199abea2c2f6d0e67ca84a53eeb99832ffe
Tobias <Tobias@localhost>
2010-10-10 20:35:05 +0200

    Fixed an issue where transactional tasks weren't properly handled by the Task Queue Celery stub.

--- src/typhoonae/taskqueue/taskqueue_celery_stub.py
+++ src/typhoonae/taskqueue/taskqueue_celery_stub.py
@@ -18,8 +18,9 @@
 from celery.task.base import Task
 
 import base64
-import celery_tasks
+import google.appengine.api.api_base_pb
 import google.appengine.api.apiproxy_stub
+import google.appengine.api.apiproxy_stub_map
 import google.appengine.api.labs.taskqueue.taskqueue_service_pb
 import google.appengine.api.labs.taskqueue.taskqueue_stub
 import google.appengine.api.urlfetch
@@ -30,6 +31,7 @@ import simplejson
 import socket
 import time
 import typhoonae.taskqueue
+import typhoonae.taskqueue.celery_tasks
 
 
 class TaskQueueServiceStub(google.appengine.api.apiproxy_stub.APIProxyStub):
@@ -60,7 +62,8 @@ class TaskQueueServiceStub(google.appengine.api.apiproxy_stub.APIProxyStub):
 
     def _SetupQueues(self):
         self.celery_tasks_for_queues = (
-            celery_tasks.create_task_queues_from_yaml(self.root_path))
+            typhoonae.taskqueue.celery_tasks.create_task_queues_from_yaml(
+                self.root_path))
 
     def _ValidQueue(self, queue_name):
         if queue_name == 'default':
@@ -68,8 +71,10 @@ class TaskQueueServiceStub(google.appengine.api.apiproxy_stub.APIProxyStub):
         return queue_name in self.celery_tasks_for_queues
 
     def _Dynamic_Add(self, request, response):
-        bulk_request = taskqueue_service_pb.TaskQueueBulkAddRequest()
-        bulk_response = taskqueue_service_pb.TaskQueueBulkAddResponse()
+        bulk_request = (google.appengine.api.labs.taskqueue.
+                        taskqueue_service_pb.TaskQueueBulkAddRequest())
+        bulk_response = (google.appengine.api.labs.taskqueue.
+                         taskqueue_service_pb.TaskQueueBulkAddResponse())
 
         bulk_request.add_add_request().CopyFrom(request)
         self._Dynamic_BulkAdd(bulk_request, bulk_response)
@@ -77,7 +82,9 @@ class TaskQueueServiceStub(google.appengine.api.apiproxy_stub.APIProxyStub):
         assert bulk_response.taskresult_size() == 1
         result = bulk_response.taskresult(0).result()
 
-        if result != taskqueue_service_pb.TaskQueueServiceError.OK:
+        OK = (google.appengine.api.labs.taskqueue.
+              taskqueue_service_pb.TaskQueueServiceError.OK)
+        if result != OK:
             raise google.appengine.runtime.apiproxy_errors.ApplicationError(
                 result)
         elif bulk_response.taskresult(0).has_chosen_task_name():
@@ -115,28 +122,51 @@ class TaskQueueServiceStub(google.appengine.api.apiproxy_stub.APIProxyStub):
                 google.appengine.api.labs.taskqueue.taskqueue_service_pb.
                 TaskQueueServiceError.UNKNOWN_QUEUE)
 
+        if request.add_request(0).has_transaction():
+            self._TransactionalBulkAdd(request)
+
         for add_request in request.add_request_list():
+            if not request.add_request(0).has_transaction():
+                content_type = 'text/plain'
+                for h in add_request.header_list():
+                    if h.key() == 'content-type':
+                        content_type = h.value()
 
-            content_type = 'text/plain'
-            for h in add_request.header_list():
-                if h.key() == 'content-type':
-                    content_type = h.value()
+                task_dict = dict(
+                    content_type=content_type,
+                    eta=add_request.eta_usec()/1000000,
+                    host=self._internal_host,
+                    method=add_request.RequestMethod_Name(add_request.method()),
+                    name=add_request.task_name(),
+                    payload=base64.b64encode(add_request.body()),
+                    port=self._internal_port,
+                    queue=add_request.queue_name(),
+                    try_count=0,
+                    url=add_request.url(),
+                )
 
-            task_dict = dict(
-                content_type=content_type,
-                eta=add_request.eta_usec()/1000000,
-                host=self._internal_host,
-                method=add_request.RequestMethod_Name(add_request.method()),
-                name=add_request.task_name(),
-                payload=base64.b64encode(add_request.body()),
-                port=self._internal_port,
-                queue=add_request.queue_name(),
-                try_count=0,
-                url=add_request.url(),
-            )
+                self._RunAsync(**task_dict)
 
-            self._RunAsync(**task_dict)
             task_result = response.add_taskresult()
+
+    def _TransactionalBulkAdd(self, request):
+        """Uses datastore.AddActions to associate tasks with a transaction.
+
+        Args:
+            request: The taskqueue_service_pb.TaskQueueBulkAddRequest
+                containing the tasks to add. N.B. all tasks in the request
+                have been validated and assigned unique names.
+        """
+        try:
+            google.appengine.api.apiproxy_stub_map.MakeSyncCall(
+                'datastore_v3', 'AddActions', request,
+                google.appengine.api.api_base_pb.VoidProto())
+        except google.appengine.runtime.apiproxy_errors.ApplicationError, e:
+            raise google.appengine.runtime.apiproxy_errors.ApplicationError(
+                e.application_error +
+                google.appengine.api.labs.taskqueue.taskqueue_service_pb.
+                TaskQueueServiceError.DATASTORE_ERROR,
+                e.error_detail)
 
     def GetQueues(self):
         """Gets all the applications's queues.

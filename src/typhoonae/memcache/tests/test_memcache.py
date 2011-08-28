e806cb13269de613245a0ae7bd8c06a33b7231c7
Tobias <Tobias@localhost>
2011-08-28 11:32:32 +0200

    Using from imports for better readability.

--- src/typhoonae/memcache/tests/test_memcache.py
+++ src/typhoonae/memcache/tests/test_memcache.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009, 2010 Tobias Rodäbel
+# Copyright 2009, 2010, 2011 Tobias Rodäbel
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,13 +13,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Unit tests for memcache."""
+"""Unit tests for the Memcache Service."""
 
-import google.appengine.api.apiproxy_stub_map
-import google.appengine.ext.db
+from google.appengine.api import apiproxy_stub_map
+from google.appengine.api import memcache
+from google.appengine.ext import db
+from typhoonae.memcache import memcache_stub
+
 import os
 import time
-import typhoonae.memcache.memcache_stub
 import unittest
 
 
@@ -27,16 +29,19 @@ class MemcacheTestCase(unittest.TestCase):
     """Testing the TyphoonAE's memcache."""
 
     def setUp(self):
-        """Register typhoonae's memcache API proxy stub."""
+        """Register TyphoonAE's memcache API proxy stub."""
 
-        google.appengine.api.apiproxy_stub_map.apiproxy = (
-            google.appengine.api.apiproxy_stub_map.APIProxyStubMap())
+        apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap()
 
-        google.appengine.api.apiproxy_stub_map.apiproxy.RegisterStub(
-            'memcache', typhoonae.memcache.memcache_stub.MemcacheServiceStub())
+        apiproxy_stub_map.apiproxy.RegisterStub('memcache',
+            memcache_stub.MemcacheServiceStub())
 
-        self.stub = google.appengine.api.apiproxy_stub_map.apiproxy.GetStub(
-            'memcache')
+        self.stub = apiproxy_stub_map.apiproxy.GetStub('memcache')
+
+    def tearDown(self):
+        """Flush memcache."""
+
+        memcache.flush_all()
 
     def testBehaviors(self):
         """Gets memcache behavior."""
@@ -48,108 +53,103 @@ class MemcacheTestCase(unittest.TestCase):
         """Adds items of different types."""
 
         foo = "bar"
-        google.appengine.api.memcache.add('foo', foo)
-        assert google.appengine.api.memcache.get('foo') == foo
+        memcache.add('foo', foo)
+        assert memcache.get('foo') == foo
 
         tres_bien = u"Très bien".encode('utf-8')
-        google.appengine.api.memcache.add('tres_bien', tres_bien)
-        assert google.appengine.api.memcache.get('tres_bien') == tres_bien
+        memcache.add('tres_bien', tres_bien)
+        assert memcache.get('tres_bien') == tres_bien
 
         items = [u'foo', 'bar', tres_bien, {1: 'one'}, 42L]
-        google.appengine.api.memcache.add('items', items)
-        assert google.appengine.api.memcache.get('items') == items
+        memcache.add('items', items)
+        assert memcache.get('items') == items
 
         number = 10
-        google.appengine.api.memcache.add('number', number)
-        assert google.appengine.api.memcache.get('number') == number
+        memcache.add('number', number)
+        assert memcache.get('number') == number
 
         long_number = long(20)
-        google.appengine.api.memcache.add('long', long_number)
-        assert google.appengine.api.memcache.get('long') == long_number
+        memcache.add('long', long_number)
+        assert memcache.get('long') == long_number
 
         yes = True
-        google.appengine.api.memcache.add('yes', yes)
-        assert google.appengine.api.memcache.get('yes') == yes
+        memcache.add('yes', yes)
+        assert memcache.get('yes') == yes
 
         greeting = 'Hello'
-        google.appengine.api.memcache.set('greeting', greeting, namespace='me')
-        assert google.appengine.api.memcache.get('greeting') is None
-        assert (google.appengine.api.memcache.get('greeting', namespace='me') ==
-            greeting)
-        assert (google.appengine.api.memcache.get('greeting', namespace='no') is
-            None)
+        memcache.set('greeting', greeting, namespace='me')
+        assert memcache.get('greeting') is None
+        assert memcache.get('greeting', namespace='me') == greeting
+        assert memcache.get('greeting', namespace='no') is None
 
         unicode_data = ['Äquator'.decode('utf-8'),]
-        google.appengine.api.memcache.set('unicode', unicode_data)
-        self.assertEqual(
-            unicode_data, google.appengine.api.memcache.get('unicode'))
-        assert type(google.appengine.api.memcache.get('unicode')) == list
+        memcache.set('unicode', unicode_data)
+        self.assertEqual(unicode_data, memcache.get('unicode'))
+        assert type(memcache.get('unicode')) == list
 
     def testDeletingItem(self):
         """Tries to set and delete a key and its value."""
 
         data = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        google.appengine.api.memcache.set('data', data)
-        assert google.appengine.api.memcache.get('data') == data
-        google.appengine.api.memcache.delete('data')
-        assert google.appengine.api.memcache.get('data') == None
+        memcache.set('data', data)
+        assert memcache.get('data') == data
+        memcache.delete('data')
+        assert memcache.get('data') == None
 
     def testDeletingUnknownKey(self):
         """Tries to delete an unknown key."""
 
-        google.appengine.api.memcache.delete('unknown')
+        memcache.delete('unknown')
 
     def testExpirationTime(self):
         """Adds an expireing item."""
 
         bye = "Good bye!"
-        google.appengine.api.memcache.add('bye', bye, 1)
-        assert google.appengine.api.memcache.get('bye') == bye
-        time.sleep(1.1)
-        assert google.appengine.api.memcache.get('bye') == None
+        memcache.add('bye', bye, 1)
+        assert memcache.get('bye') == bye
+        time.sleep(2)
+        assert memcache.get('bye') == None
 
     def testGetKey(self):
         """Tries to obtain a key."""
 
         os.environ['APPLICATION_ID'] = ''
-        assert typhoonae.memcache.memcache_stub.getKey('bar') == 'YmFy'
-        assert (typhoonae.memcache.memcache_stub.getKey('b', namespace='a') ==
-                'YS5i')
+        assert memcache_stub.getKey('bar') == 'YmFy'
+        assert (memcache_stub.getKey('b', namespace='a') == 'YS5i')
         os.environ['APPLICATION_ID'] = 'app'
-        assert (typhoonae.memcache.memcache_stub.getKey('b', namespace='a') ==
-                'YXBwLmEuYg==')
+        assert (memcache_stub.getKey('b', namespace='a') == 'YXBwLmEuYg==')
         del os.environ['APPLICATION_ID']
-        google.appengine.api.memcache.set('counter', 0, namespace='me') 
-        assert google.appengine.api.memcache.get('counter', namespace='me') == 0
+        memcache.set('counter', 0, namespace='me') 
+        assert memcache.get('counter', namespace='me') == 0
 
     def testIncrementDecrement(self):
         """Testing automatically incrementing and decrementing."""
 
-        google.appengine.api.memcache.incr('unknown_key')
-        assert google.appengine.api.memcache.get('unknown_key') == None
-        google.appengine.api.memcache.set('counter', 0) 
-        assert google.appengine.api.memcache.get('counter') == 0
-        google.appengine.api.memcache.incr('counter')
-        assert google.appengine.api.memcache.get('counter') == 1
-        google.appengine.api.memcache.incr('counter', delta=2)
-        assert google.appengine.api.memcache.get('counter') == 3
-        google.appengine.api.memcache.decr('counter')
-        assert google.appengine.api.memcache.get('counter') == 2
-        google.appengine.api.memcache.decr('counter', 2)
-        assert google.appengine.api.memcache.get('counter') == 0
-        google.appengine.api.memcache.incr('second_counter', initial_value=10)
-        assert google.appengine.api.memcache.get('second_counter') == 11
-        google.appengine.api.memcache.decr('third_counter', initial_value=10)
-        assert google.appengine.api.memcache.get('third_counter') == 9
+        memcache.incr('unknown_key')
+        assert memcache.get('unknown_key') == None
+        memcache.set('counter', 0) 
+        assert memcache.get('counter') == 0
+        memcache.incr('counter')
+        assert memcache.get('counter') == 1
+        memcache.incr('counter', delta=2)
+        assert memcache.get('counter') == 3
+        memcache.decr('counter')
+        assert memcache.get('counter') == 2
+        memcache.decr('counter', 2)
+        assert memcache.get('counter') == 0
+        memcache.incr('second_counter', initial_value=10)
+        assert memcache.get('second_counter') == 11
+        memcache.decr('third_counter', initial_value=10)
+        assert memcache.get('third_counter') == 9
 
         # This should cause an error message, because zero deltas are not
         # allowed.
-        google.appengine.api.memcache.incr('counter', delta=0)
+        memcache.incr('counter', delta=0)
 
-        google.appengine.api.memcache.set('lcounter', long(20))
-        assert google.appengine.api.memcache.get('lcounter') == long(20)
-        google.appengine.api.memcache.incr('lcounter')
-        assert google.appengine.api.memcache.get('lcounter') == long(21)
+        memcache.set('lcounter', long(20))
+        assert memcache.get('lcounter') == long(20)
+        memcache.incr('lcounter')
+        assert memcache.get('lcounter') == long(21)
 
     def testUnsuccessfulIncrement(self):
         """Tests incrementing values in a broken chache."""
@@ -157,63 +157,62 @@ class MemcacheTestCase(unittest.TestCase):
         cache = self.stub._cache
         self.stub._cache = {}
 
-        google.appengine.api.memcache.incr('somekey')
+        memcache.incr('somekey')
 
         self.stub._cache = cache
-        del cache
 
     def testBatchIncrement(self):
         """Tests incrementing multiple keys with integer values."""
 
-        google.appengine.api.memcache.set('low', 0)
-        google.appengine.api.memcache.set('high', 100)
+        memcache.set('low', 0)
+        memcache.set('high', 100)
 
-        google.appengine.api.memcache.offset_multi({'low': 1, 'high': -50})
+        memcache.offset_multi({'low': 1, 'high': -50})
 
-        self.assertEqual(1, google.appengine.api.memcache.get('low'))
-        self.assertEqual(50, google.appengine.api.memcache.get('high'))
+        self.assertEqual(1, memcache.get('low'))
+        self.assertEqual(50, memcache.get('high'))
 
-        google.appengine.api.memcache.offset_multi({'low': 9, 'high': 0})
+        memcache.offset_multi({'low': 9, 'high': 0})
 
-        self.assertEqual(10, google.appengine.api.memcache.get('low'))
-        self.assertEqual(50, google.appengine.api.memcache.get('high'))
+        self.assertEqual(10, memcache.get('low'))
+        self.assertEqual(50, memcache.get('high'))
 
-        google.appengine.api.memcache.offset_multi(
+        memcache.offset_multi(
             {'max': 5, 'min': -5}, initial_value=10)
 
-        self.assertEqual(15, google.appengine.api.memcache.get('max'))
-        self.assertEqual(5, google.appengine.api.memcache.get('min'))
+        self.assertEqual(15, memcache.get('max'))
+        self.assertEqual(5, memcache.get('min'))
 
     def testFlushAll(self):
         """Flushes the whole cache."""
 
         spam = "Hello, World!"
-        google.appengine.api.memcache.set('spam', spam)
-        assert google.appengine.api.memcache.get('spam') == spam
-        google.appengine.api.memcache.flush_all()
-        assert google.appengine.api.memcache.get('spam') == None
+        memcache.set('spam', spam)
+        assert memcache.get('spam') == spam
+        memcache.flush_all()
+        assert memcache.get('spam') == None
 
     def testReplaceItem(self):
         """Adds and replaces a cached item."""
 
         first = "Little pig, little pig, let me come in!"
         second = "Not by the hair on my chinny-chin-chin!"
-        google.appengine.api.memcache.set('first', first)
-        assert google.appengine.api.memcache.get('first') == first
-        google.appengine.api.memcache.replace('first', second)
-        assert google.appengine.api.memcache.get('first') == second
+        memcache.set('first', first)
+        assert memcache.get('first') == first
+        memcache.replace('first', second)
+        assert memcache.get('first') == second
 
     def testClient(self):
         """Tests the class-based Memcache interface."""
 
-        client = google.appengine.api.memcache.Client()
+        client = memcache.Client()
         client.set('foobar', 'some value')
         assert client.get('foobar') == 'some value'
 
     def testComapareAndSet(self):
         """Tests the Compare-And-Set method."""
 
-        client = google.appengine.api.memcache.Client() 
+        client = memcache.Client() 
         client.set('mycounter', 0)
 
         def bump_counter(key): 
@@ -231,44 +230,37 @@ class MemcacheTestCase(unittest.TestCase):
     def testCacheProtobuf(self):
         """Tries to cache an encoded protocol buffer."""
 
-        class MyModel(google.appengine.ext.db.Model):
-            name = google.appengine.ext.db.StringProperty()
+        from google.appengine.ext import db
+
+        class MyModel(db.Model):
+            name = db.StringProperty()
 
         entity = MyModel(name="foobar")
 
         os.environ['APPLICATION_ID'] = 'app'
-        google.appengine.api.memcache.set('protobuf',
-            google.appengine.ext.db.model_to_protobuf(entity).Encode())
+        memcache.set('protobuf', db.model_to_protobuf(entity).Encode())
 
-        encoded_entity = google.appengine.api.memcache.get('protobuf')
-        cached_entity = google.appengine.ext.db.model_from_protobuf(
-            encoded_entity)
+        encoded_entity = memcache.get('protobuf')
+        cached_entity = db.model_from_protobuf(encoded_entity)
         assert cached_entity.name == 'foobar'
 
     def testMulti(self):
         """Stores multiple keys' values at once."""
 
-        google.appengine.api.memcache.set_multi(
-            {'map_key_one': 1, 'map_key_two': u'some value'})
-        values = google.appengine.api.memcache.get_multi(
-            ['map_key_one', 'map_key_two'])
+        memcache.set_multi({'map_key_one': 1, 'map_key_two': u'some value'})
+        values = memcache.get_multi(['map_key_one', 'map_key_two'])
         assert {'map_key_one': 1, 'map_key_two': u'some value'} == values
 
-        google.appengine.api.memcache.add_multi(
+        memcache.add_multi(
             {'map_key_one': 'one', 'map_key_two': 2, 'three': u'trois'})
-        values = google.appengine.api.memcache.get_multi(
-            ['map_key_two', 'three'])
+        values = memcache.get_multi(['map_key_two', 'three'])
         assert {'map_key_two': u'some value', 'three': u'trois'} == values
 
     def testStats(self):
         """Tries to get memcache stats."""
 
-        stats = google.appengine.api.memcache.get_stats()
+        stats = memcache.get_stats()
         self.assertEqual(
             set(['hits', 'items', 'bytes', 'oldest_item_age', 'misses',
                  'byte_hits']),
             set(stats.keys()))
- 
-
-if __name__ == "__main__":
-    unittest.main()
